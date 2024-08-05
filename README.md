dd0da0285f39a379ef136be455b7b42dd3ac4170
tomassw1623 <tomassw161623@gmail.com>
2024-08-05 02:45:06 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# backend-test-cicd
+# backend-cicd-0test
---
+++ src/main/java/com/example/codepipeline/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.codepipeline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/sayHello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "Hi This Page Before Modifiyed";
+    }
+
+
+}
